037c2c0f17a922a91683aee690366c4c516a0641
Eliot <eliot.meurillon@gmail.com>
2023-09-20 15:43:44 +0200

    feat: add post user

--- src/user/user.json-service.ts
+++ src/user/user.json-service.ts
@@ -1,12 +1,32 @@
 import { User } from './user';
 import { UserService } from './user.service';
+import * as fs from 'fs';
+
+const FILE_PATH = './src/user/users.json';
 
 export class UserJSONService implements UserService {
     add(username: string): User {
-        throw new Error('Method not implemented.');
+        const rawData = fs.readFileSync(FILE_PATH, 'utf8');
+        const users: User[] = JSON.parse(rawData);
+
+        if (users.some((u) => u.username === username)) {
+            throw new Error('Username already exists.');
+        }
+
+        const maxId = users.reduce((acc, user) => Math.max(acc, user.id), 0);
+        const newUser: User = { id: maxId + 1, username };
+
+        users.push(newUser);
+        fs.writeFileSync(FILE_PATH, JSON.stringify(users, null, 2));
+
+        return newUser;
     }
 
     getById(id: number): User | null {
-        throw new Error('Method not implemented.');
+        const rawData = fs.readFileSync(FILE_PATH, 'utf8');
+        const users: User[] = JSON.parse(rawData);
+
+        let user = users.find((u) => u.id === id);
+        return user || null;
     }
 }
